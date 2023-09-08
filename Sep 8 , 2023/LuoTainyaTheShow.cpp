061d127af6b314d3a5f9009750547e021fef9d43
Ayush Yadav <aayush0029@gmail.com>
2023-09-08 22:55:43 +0530

    Second Day

--- Sep 8 , 2023/LuoTainyaTheShow.cpp
+++ Sep 8 , 2023/LuoTainyaTheShow.cpp
@@ -1,42 +1,58 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
-#define int long long
-
-
-void solve(){
-    int n , m;
-    cin >> n >> m;
-    int arr[n+1];
-    int left = n;
-    int right = 1;
-    map<int,int>mp;
-    for(int i = 1 ; i <= n; i++){
-        cin >> arr[i];
-        if(arr[i]>0){
-            mp[arr[i]] = i;
-            left = min(left,arr[i]);
-            right = max(right,arr[i]);
+#define int long long int
+signed main(){
+   int t;cin>>t;
+   while ( t--)
+   {
+        int n,m;cin>>n>>m;
+        vector<int>v(n);
+        int one=0;
+        int two=0;
+        vector<int>res(m+1);
+        int cnt=0;
+        for ( int i = 0; i <  n; i++)
+        {
+             cin>>v[i];
+             if(v[i]==-1){
+                one++;
+             }
+            else if(v[i]==-2){
+                two++;
+             }
+             else
+             {    
+                if(res[v[i]]==0){
+                    cnt++;
+                }
+                 res[v[i]]=1;
+             }
         }
-    }
-    for(int i = 1 ; i<=n ; i++){
-        if(arr[i] < 0)
-    }
-}
 
-int32_t main(){
+        vector<int>pre(m+1),suf(m+1);
+        suf[m]=res[m];
+        for(int i=m-1;i>=1;i--)
+        {
+            suf[i]=suf[i+1]+res[i];
+        }
+        pre[1]=res[1];
+        for(int i=2;i<=m;i++){
+            pre[i]=pre[i-1]+res[i];
+        }
 
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif
-    IOS;
-    int t = 1;
-    cin>>t;
-    while(t--){
-        solve();
-        cout<<endl;
-    }
-    return 0;
+        int ans=min(m,one+cnt);
+       int ans2=min(m,two+cnt);
+       // cout<<ans<<endl;
+        ans=max(ans,ans2);
+
+        for(int i=1;i<=m;i++){
+            if(res[i]==1){
+                int ans1=1+min(i-1,pre[i-1]+one)+min(m-i,suf[i+1]+two);
+                ans=max(ans,ans1);
+            }
+        }
+
+        cout<<ans<<endl;   
+   }
+   
 }
